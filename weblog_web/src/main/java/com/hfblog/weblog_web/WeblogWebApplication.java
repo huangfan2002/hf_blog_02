5b1dac3cb3ef53f4ffdb25733782eea3a11a2476
fan huang <huangfan_email@njust.edu.cn>
2025-06-26 15:33:03 +0800

    web_blog多模块文件

---
+++ weblog_web/src/main/java/com/hfblog/weblog_web/WeblogWebApplication.java
@@ -0,0 +1,13 @@
+package com.hfblog.weblog_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeblogWebApplication {
+//11111
+    public static void main(String[] args) {
+        SpringApplication.run(WeblogWebApplication.class, args);
+    }
+
+}
